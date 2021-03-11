61e5b947556fd5ccdc8e466fe045ee843540fc70
lucaAngerri <luca.angerri@mataro.epiaedu.cat>
2021-03-11 15:58:45 +0100

    he crat el arxiu index.php

---
+++ uytransfer/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>PHP</title>
+		<link rel="stylesheet" type="text/css" href="css.estilos.css">
+		<meta name="viewport" content="width=device-width, inicial-scale=1.0">
+</head>
+<body>
+
+	<?php
+		$color = "red";
+		echo "<span style=\"color:$color;\">Estoy doramion</span>";
+	?>
+
+</body>
+</html>
+
